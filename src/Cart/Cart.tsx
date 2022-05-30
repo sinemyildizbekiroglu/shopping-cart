1e9ff1d1287788bf27a83282725a85ab0c0bff25
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-05-30 10:09:15 +0200

    Add props to `Cart.tsx`

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -5,3 +5,23 @@ import { Wrapper } from "./Cart.styles"
 
 // Types
 import { CartItemType } from "../App"
+
+type Props = {
+  cartItems: CartItemType[]
+  addToCart: (clickedItem: CartItemType) => void
+  removeFromCart: (id: number) => void
+}
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  return (
+    <Wrapper>
+      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+      {cartItems.map((item) => (
+        <CartItem />
+      ))}
+    </Wrapper>
+  )
+}
+
+export default Cart
